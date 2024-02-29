b7cb1210c5ea06031d4bd186908ac007abd6d629
gitusergb <g4ur131@gmail.com>
2024-02-29 13:36:49 +0530

    dp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import DatePicker from './DatePicker';
+import DatePicker1 from './DatePicker';
 import DatePicker from "react-datepicker";
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
@@ -7,29 +7,24 @@ import "react-datepicker/dist/react-datepicker.css";
 function App() {
   const [startDate, setStartDate] = useState(new Date());
 
-  // const [selectedDate, setSelectedDate] = useState(new Date());
-  // const handleDateChange = (date) => {
-  //   setSelectedDate(date);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
   
-  // };
+  };
 
 
   return (
     <div className="App">
        <h1>Select a Date</h1>
-      {/* <DatePicker
+      <DatePicker1
         selectedDate={selectedDate}
         onDateChange={handleDateChange}
         dateFormat="MM/DD/YYYY"
         minDate={new Date()}
         maxDate={new Date(new Date().getFullYear() + 1, 11, 31)}
-      /> */}
-       {/* selectedDate,
-  onDateChange,
-  dateFormat = 'MM/DD/YYYY',
-  minDate,
-  maxDate,
-  className, */}
+      />
+ 
  <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
     </div>
   );
